4a99aab58197665bcdf02d6aeff259e80e2e1ecb
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2020-08-18 15:05:33 -0400

    #2 Fixed error that was preventing Optional non generic types. Added in choices by supporting Enums as a katra type.
    Signed-off-by: Nicholas Cilfone <nicholas.cilfone@fmr.com>

--- spock/backend/attr/typed.py
+++ spock/backend/attr/typed.py
@@ -6,6 +6,7 @@
 """Handles the definitions of arguments types for Spock (backend: attrs)"""
 
 import attr
+from enum import EnumMeta
 from typing import _GenericAlias
 from typing import Union
 
@@ -48,7 +49,7 @@ def _generic_alias_katra(typed, default=None, optional=False):
 
     A 'katra' is the basic functional unit of `spock`. It defines a parameter using attrs as the backend, type checks
     both simple types and subscripted GenericAlias types (e.g. lists and tuples), handles setting default parameters,
-    and deals parameter optionality
+    and deals with parameter optionality
 
     Handles: List[type] and Tuple[type]
 
@@ -66,18 +67,60 @@ def _generic_alias_katra(typed, default=None, optional=False):
     # base python class from which a GenericAlias is derived
     base_typed = typed.__origin__
     if default is not None:
-        # if a default is provided, that takes precedence
-        x = attr.ib(validator=[attr.validators.instance_of(base_typed), _validate_subscripted_generic],
-                    default=default, type=base_typed, metadata={'type': _extract_base_type(typed),
-                                                                'base': typed._name})
+        x = attr.ib(validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of(typed.__args__[0]),
+            iterable_validator=attr.validators.instance_of(base_typed)
+        ), type=base_typed, default=default, metadata={'type': _extract_base_type(typed), 'base': typed._name})
     elif optional:
         # if there's no default, but marked as optional, then set the default to None
-        x = attr.ib(validator=[attr.validators.optional(attr.validators.instance_of(base_typed)),
-                               _validate_subscripted_generic], type=base_typed, default=default,
-                    metadata={'type': _extract_base_type(typed), 'optional': True, 'base': typed._name})
+        x = attr.ib(validator=attr.validators.optional(attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of(typed.__args__[0]),
+            iterable_validator=attr.validators.instance_of(base_typed)
+        )), type=base_typed, default=default,
+            metadata={'type': _extract_base_type(typed), 'optional': True, 'base': typed._name})
     else:
-        x = attr.ib(validator=[attr.validators.instance_of(base_typed), _validate_subscripted_generic],
-                    type=base_typed, metadata={'type': _extract_base_type(typed), 'base': typed._name})
+        x = attr.ib(validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of(typed.__args__[0]),
+            iterable_validator=attr.validators.instance_of(base_typed)
+        ), type=base_typed, metadata={'type': _extract_base_type(typed), 'base': typed._name})
+    return x
+
+
+def _enum_katra(typed, default=None, optional=False):
+    """Private interface to create a Enum typed katra
+
+    A 'katra' is the basic functional unit of `spock`. It defines a parameter using attrs as the backend, type checks
+    both simple types and subscripted GenericAlias types (e.g. lists and tuples), handles setting default parameters,
+    and deals with parameter optionality
+
+    *Args*:
+
+        typed: the type of the parameter to define
+        default: the default value to assign if given
+        optional: whether to make the parameter optional or not (thus allowing None)
+
+    *Returns*:
+
+        x: Attribute from attrs
+
+    """
+    # First check if the types of Enum are the same
+    type_set = {type(val.value) for val in typed}
+    if len(type_set) > 1:
+        raise TypeError(f"Enum cannot be defined with multiple types: {type_set}")
+    base_type = list(type_set)[-1]
+    allowed = [val.value for val in typed]
+    if default is not None:
+        x = attr.ib(
+            validator=[attr.validators.instance_of(base_type), attr.validators.in_(allowed)],
+            default=default, type=base_type, metadata={'base': typed.__name__})
+    elif optional:
+        x = attr.ib(
+            validator=attr.validators.optional([attr.validators.instance_of(base_type), attr.validators.in_(allowed)]),
+            default=default, type=base_type, metadata={'base': typed.__name__})
+    else:
+        x = attr.ib(validator=[attr.validators.instance_of(base_type), attr.validators.in_(allowed)], type=typed,
+                    metadata={'base': typed.__name__})
     return x
 
 
@@ -86,7 +129,7 @@ def _type_katra(typed, default=None, optional=False):
 
     A 'katra' is the basic functional unit of `spock`. It defines a parameter using attrs as the backend, type checks
     both simple types and subscripted GenericAlias types (e.g. lists and tuples), handles setting default parameters,
-    and deals parameter optionality
+    and deals with parameter optionality
 
     Handles: bool, string, float, int, List, and Tuple
 
@@ -107,9 +150,10 @@ def _type_katra(typed, default=None, optional=False):
         default = False
     if default is not None:
         # if a default is provided, that takes precedence
-        x = attr.ib(validator=attr.validators.instance_of(typed), default=default, metadata={'base': typed.__name__})
+        x = attr.ib(validator=attr.validators.instance_of(typed), default=default, type=typed,
+                    metadata={'base': typed.__name__})
     elif optional:
-        x = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(typed)), default=default,
+        x = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(typed)), default=default, type=typed,
                     metadata={'optional': True, 'base': typed.__name__})
     else:
         x = attr.ib(validator=attr.validators.instance_of(typed), type=typed, metadata={'base': typed.__name__})
@@ -131,23 +175,24 @@ def _handle_optional_typing(typed):
         typed: type (modified if Optional)
         optional: boolean for katra creation
     """
-    # Check the length of type __args__
-    # If it is more than one than it is most likely optional but check against NoneType in the tuple to verify
-    type_args = typed.__args__
-    # Optional[X] has type_args = (X, None) and is equal to Union[X, None]
-    if (len(type_args) == 2) and (typed == Union[type_args[0], None]):
-        # Since this is true we need to strip out the OG type
-        # Grab all the types that are not NoneType and collapse to a list
-        type_list = [val for val in type_args if val is not type(None)]
-        if len(type_list) > 1:
-            raise TypeError(f"Passing multiple subscript types to GenericAlias is not supported: {type_list}")
-        else:
-            typed = type_list[0]
-        # Set the optional flag to true
-        optional = True
-    else:
-        # Untrue so it's not optional
-        optional = False
+    # Set optional to false
+    optional = False
+    # Check if it has __args__ to look for optionality as it is a GenericAlias
+    if hasattr(typed, '__args__'):
+        # If it is more than one than it is most likely optional but check against NoneType in the tuple to verify
+        # Check the length of type __args__
+        type_args = typed.__args__
+        # Optional[X] has type_args = (X, None) and is equal to Union[X, None]
+        if (len(type_args) == 2) and (typed == Union[type_args[0], None]):
+            # Since this is true we need to strip out the OG type
+            # Grab all the types that are not NoneType and collapse to a list
+            type_list = [val for val in type_args if val is not type(None)]
+            if len(type_list) > 1:
+                raise TypeError(f"Passing multiple subscript types to GenericAlias is not supported: {type_list}")
+            else:
+                typed = type_list[0]
+            # Set the optional flag to true
+            optional = True
     return typed, optional
 
 
@@ -175,6 +220,8 @@ def katra(typed, default=None):
     # If it is subscript typed it will not be T which python uses as a generic type name
     if isinstance(typed, _GenericAlias) and typed.__args__[0].__name__ != "T":
         x = _generic_alias_katra(typed=typed, default=default, optional=optional)
+    elif isinstance(typed, EnumMeta):
+        x = _enum_katra(typed=typed, default=default, optional=optional)
     else:
         x = _type_katra(typed=typed, default=default, optional=optional)
     return x
--- tests/debug.py
+++ tests/debug.py
@@ -5,12 +5,21 @@ from spock.config import spock
 from spock.config import spock_config
 from typing import List
 from typing import Optional
+from enum import Enum
 from spock.builder import ConfigArgBuilder
+
+
+class Choice(Enum):
+    pear = 'pear'
+    banana = 'banana'
+
 
 @spock
 class Test:
+    new_choice: Optional[Choice]
+    new: int
     other: Optional[int]
-    value: Optional[List[int]]
+    value: Optional[List[int]] = [1, 2]
 
 
 @spock_config
@@ -21,8 +30,9 @@ class Test2:
 
 def main():
     # test = Test()
-    # attrs_class = ConfigArgBuilder(Test).save(user_specified_path='/tmp/', file_extension='.yaml').generate()
-    dc_class = ConfigArgBuilder(Test2).save(user_specified_path='/tmp', file_extension='.yaml').generate()
+    attrs_class = ConfigArgBuilder(Test).generate()
+    print(attrs_class)
+    # dc_class = ConfigArgBuilder(Test2).save(user_specified_path='/tmp', file_extension='.yaml').generate()
 
 
 if __name__ == '__main__':
