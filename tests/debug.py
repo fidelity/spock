245690caaf3c6e8d1ac603770210691d8daf7ac1
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2020-10-05 13:20:31 -0400

    Fixed bug that was causing removal of parameter definitions when converting lists to tuples. New function to recursively update a dictionary which prevents this from occurring.
    Signed-off-by: Nicholas Cilfone <nicholas.cilfone@fmr.com>

--- tests/debug.py
+++ tests/debug.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from spock.backend.dataclass.args import IntArg, ListArg, IntOptArg, ChoiceArg, SavePathOptArg
 from spock.config import spock
 from spock.config import spock_config
@@ -14,6 +13,83 @@ from spock.backend.attr.typed import SavePath
 class Choice(Enum):
     pear = 'pear'
     banana = 'banana'
+
+
+class Features(Enum):
+    mfcc = 'mfcc'
+    spectrogram = 'spectrogram'
+    logfbank = 'logfbank'
+
+
+class Window(Enum):
+    hann = 'hann'
+    hamming = 'hamming'
+
+
+
+@spock
+class LoaderConfig:
+    batch_size: int
+    drop_odd_batch: bool
+    shuffle: bool
+    n_workers: Optional[int] = 0
+    distributed: bool
+
+
+@spock
+class ManifestConfig:
+    manifest_path: str
+    wav_name: str
+    transcript_name: str
+    duration_name: str
+    sample_name: str
+    bitrate_name: str
+    min_duration: Optional[float]
+    max_duration: Optional[float]
+    sorted: bool
+
+
+@spock
+class TrainingManifestConfig(ManifestConfig):
+    ...
+
+
+@spock
+class ValidationManifestConfig(ManifestConfig):
+    ...
+
+
+@spock
+class TranscriptConfig:
+    vocab: str
+
+
+@spock
+class AudioConfig:
+    root_dir: str
+    features: Features
+    dither: Optional[float]
+    trim_silence: bool
+    normalize_signal: bool
+    gain: Optional[float]
+    per_feature_norm: bool
+    window_ms: int
+    stride_ms: int
+    pre_emphasis: float
+    n_fft: int
+    window_fnc: Window
+    speed_perturb: Optional[List[float]]
+    n_features: Optional[int]
+
+
+@spock
+class TrainingAudioConfig(AudioConfig):
+    ...
+
+
+@spock
+class ValidationAudioConfig(AudioConfig):
+    ...
 
 
 @spock
@@ -41,7 +117,11 @@ class Test:
 
 def main():
     # test = Test()
-    attrs_class = ConfigArgBuilder(Test).generate()
+
+    attrs_class = ConfigArgBuilder(TrainingManifestConfig, TranscriptConfig,
+                                   TrainingAudioConfig, LoaderConfig,
+                                   ValidationAudioConfig, ValidationManifestConfig
+                                   ).generate()
     print(attrs_class)
     # dc_class = ConfigArgBuilder(OldInherit).generate()
     # print(dc_class)
