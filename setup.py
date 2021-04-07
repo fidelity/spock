5fd980969d3e0a38ef4c5f893b04dba3ea510380
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2021-04-07 13:52:10 -0400

    Updating setup.py with correct information (#54)

--- setup.py
+++ setup.py
@@ -20,21 +20,28 @@ setuptools.setup(
     cmdclass=versioneer.get_cmdclass(),
     author="FMR LLC",
     url="https://github.com/fidelity/spock",
+    download_url="https://github.com/fidelity/spock",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     project_urls={
         "Source": "https://github.com/fidelity/spock",
-        "Documentation": "https://fidelity.github.io/spock/"
+        "Documentation": "https://fidelity.github.io/spock/",
+        "Bug Tracker": "https://fidelity.github.io/spock/issues"
     },
-    keywords="dataclass config argparser parameter configuration",
+    keywords=['configuration', 'argparse', 'parameters', 'machine learning', 'deep learning', 'reproducibility'],
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     python_requires='>=3.6',
     install_requires=install_reqs
