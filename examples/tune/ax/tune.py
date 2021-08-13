5b0779cbbbcfc7c39946ad5ab599f6bd40d0b4fd
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2021-08-13 11:52:19 -0400

    update example

--- examples/tune/ax/tune.py
+++ examples/tune/ax/tune.py
@@ -39,7 +39,7 @@ def main():
     # Split the Iris data
     X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-    # Optuna config -- this will internally spawn the study object for the define-and-run style which will be returned
+    # Ax config -- this will internally spawn the AxClient service API style which will be returned
     # by accessing the tuner_status property on the ConfigArgBuilder object
     ax_config = AxTunerConfig(
         objective_name='accuracy',
@@ -47,7 +47,7 @@ def main():
     )
 
     # Use the builder to setup
-    # Call tuner to indicate that we are going to do some HP tuning -- passing in an optuna study object
+    # Call tuner to indicate that we are going to do some HP tuning -- passing in an ax study object
     attrs_obj = (
         ConfigArgBuilder(
             LogisticRegressionHP,
@@ -62,12 +62,13 @@ def main():
     # prior to starting the sampling process
     fixed_params = attrs_obj.generate()
 
-    # Now we iterate through a bunch of optuna trials
+    # Now we iterate through a bunch of ax trials
     for _ in range(fixed_params.BasicParams.n_trials):
         # The crux of spock support -- call save w/ the add_tuner_sample flag to write the current draw to file and
         # then call sample to return the composed Spockspace of the fixed parameters and the sampled parameters
-        # Under the hood spock uses the define-and-run Optuna interface -- thus it handled the underlying 'ask' call
-        # and returns the necessary trial object in the return dictionary to call 'tell' with the study object
+        # Under the hood spock uses the AxClient Ax interface -- thus it handled the underlying call to get the next
+        # sample and returns the necessary AxClient object in the return dictionary to call 'complete_trial' with the
+        # associated metrics
         hp_attrs = attrs_obj.save(
             add_tuner_sample=True, user_specified_path="/tmp/ax"
         ).sample()
@@ -81,8 +82,8 @@ def main():
         val_acc = clf.score(X_valid, y_valid)
         # Get the status of the tuner -- this dict will contain all the objects needed to update
         tuner_status = attrs_obj.tuner_status
-        # Pull the study and trials object out of the return dictionary and pass it to the tell call using the study
-        # object
+        # Pull the AxClient object and trial index out of the return dictionary and call 'complete_trial' on the
+        # AxClient object with the correct raw_data that contains the objective name
         tuner_status['client'].complete_trial(
             trial_index=tuner_status['trial_index'],
             raw_data={'accuracy': (val_acc, 0.0)}
