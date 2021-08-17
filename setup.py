50bff458b3553fe203913551368155e7e04e9592
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2021-08-17 11:55:11 -0400

    bumped some docs

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setuptools.setup(
         "machine learning",
         "deep learning",
         "reproducibility",
-        "hyper-parameter tuning"
+        "hyper-parameter tuning",
     ],
     packages=setuptools.find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
