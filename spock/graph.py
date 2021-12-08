4198bb5b0b7bed69d60c5cb88c9f32a1477436a6
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2021-12-08 13:41:25 -0500

    removing networkx dep

--- spock/graph.py
+++ spock/graph.py
@@ -6,17 +6,15 @@
 """Handles creation and ops for DAGs"""
 
 import attr
-from copy import deepcopy
 from spock.utils import _is_spock_instance, _check_iterable
 from enum import EnumMeta
-import networkx
 
 
 class Graph(object):
     def __init__(self, input_classes):
         self._input_classes = input_classes
         # Build
-        self._dag: networkx.DiGraph = self._build()
+        self._dag = self._build()
         # Validate (No cycles in DAG)
         if self._has_cycles() is True:
             raise ValueError(
@@ -30,19 +28,18 @@ class Graph(object):
 
     @property
     def roots(self):
-        return [n for n, d in self._dag.in_degree() if d==0]
-
-    def out_edges(self, source):
-        return self._dag.out_edges(source)
+        return [k for k, v in self._dag.items() if len(v) == 0]
 
     def _build(self):
         # Build a dictionary of all nodes (base spock classes)
-        dag = networkx.DiGraph()
-
-        for spock_cls in self._input_classes:
-            self._recurse_spock_class(dag, spock_cls)
-
-        return dag
+        nodes = {val: [] for val in self._input_classes}
+        # Iterate thorough all of the base spock classes to get the dependencies and reverse dependencies
+        for input_class in self._input_classes:
+            dep_classes = self._find_all_spock_classes(input_class)
+            for v in dep_classes:
+                nodes.get(v).append(input_class)
+        nodes = {key: set(val) for key, val in nodes.items()}
+        return nodes
 
     def _recurse_spock_class(self, dag, spock_cls):
         dep_classes = self._find_all_spock_classes(spock_cls)
@@ -82,8 +79,32 @@ class Graph(object):
         return [v.value for v in enum_obj if _is_spock_instance(v.value)]
 
     def _has_cycles(self):
-        try:
-            networkx.algorithms.cycles.find_cycle(self._dag)
-            return True
-        except networkx.NetworkXNoCycle:
-            return False
+        # DFS w/ recursion stack for DAG cycle detection
+        visited = {key: False for key in self._dag.keys()}
+        all_nodes = list(visited.keys())
+        recursion_stack = {key: False for key in self._dag.keys()}
+        # Recur for all edges
+        for node in all_nodes:
+            if visited.get(node) is False:
+                # Surface the recursive checks
+                if self._cycle_dfs(node, visited, recursion_stack) is True:
+                    return True
+        return False
+
+    def _cycle_dfs(self, node, visited, recursion_stack):
+        # Update the visited nodes
+        visited.update({node: True})
+        # Update recursion stack
+        recursion_stack.update({node: True})
+        # Recur through the edges
+        for val in self._dag.get(node):
+            if visited.get(val) is False:
+                # The the recursion returns True then work it up the stack
+                if self._cycle_dfs(val, visited, recursion_stack) is True:
+                    return True
+            # If the vertex is already in the recursion stack then we have a cycle
+            elif recursion_stack.get(val) is True:
+                return True
+        # Reset the stack for the current node if we've completed the DFS from this node
+        recursion_stack.update({node: False})
+        return False
