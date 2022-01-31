3142b129c19944a1970e5d3418678ebbee4437ea
Nicholas Cilfone <nicholas.cilfone@fmr.com>
2022-01-31 10:12:09 -0500

    fixed missing imports to keep SavePath on the correct part of the API for backwards compat

--- spock/__init__.py
+++ spock/__init__.py
@@ -11,11 +11,12 @@ Please refer to the documentation provided in the README.md
 
 from spock._version import get_versions
 from spock.builder import ConfigArgBuilder
+from spock.backend.typed import SavePath
 from spock.config import spock
 
 SpockBuilder = ConfigArgBuilder
 
-__all__ = ["args", "builder", "config", "spock", "SpockBuilder"]
+__all__ = ["args", "builder", "config", "SavePath", "spock", "SpockBuilder"]
 
 __version__ = get_versions()["version"]
 del get_versions
